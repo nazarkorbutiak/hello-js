38fcd5035e094d4055d87edf1eb14f3108302060
nazarkorbutiak <korbutaknazar@gmail.com>
2020-02-18 18:52:33 +0200

    animations do not true

--- public/box/movebox.js
+++ public/box/movebox.js
@@ -14,7 +14,65 @@ var board = [
 
 var elBoard = document.querySelectorAll( 'table#move-the-box td' );
 var selBox = [];
+
+var animationCounter = 0;
+
+
+
+function boomH() {
+    let Boom = false;
+    for ( let r=bRow-1; r>=0; r-- ){
+        for (let c=0; c<bCol-1; c++){
+            if ( board[r][c]>0){
+                let l=1
+                for ( let cc=c+1; cc<bCol; cc++ ){
+                    if ( board[r][cc] != board[r][c] ){
+                        break;
+                    }
+                    l++
+                }
+                if ( l>z ) {
+                    for ( let cc=c; cc<c+l; cc++){
+                        board[r][c]
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+
+//- Має щось Бути!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var Boom = false;
+for ( let c= 0; c<bCol; c++ ){
+    for ( let r=bRow-1; r>0; r--){
+        if( board[r][c]>0 ){
+            let l=1;
+            //for (){
+
+            //}
+        }
+    }
+}
+//- Має щось Бути!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+
+
+
+function boom() {
+    let bH = boomH();
+    let bV = boomV();
+    return ( bH || bV );
+}//- Має щось Бути!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
 function drawBoard() {
+    clearBoom();
+    gravitation();
+    while ( animationCounter>0) {};
+    let Boom = boom();
     for ( let r = 0; r < bRow; r++ ) {
         for ( let c=0; c< bCol; c++ ){
             let i = bCol * r +c;
@@ -24,6 +82,10 @@ function drawBoard() {
                 elBoard[i].className ='box-' + board[r][c];
             }
         }
+    }
+    if( Boom ){
+        setTimeout( function(){clearBoom();}, 500)
+        //schosi mae butu!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 }
     
@@ -55,6 +117,15 @@ function moveLeft() {
     }
 }
 function moveUp() {
+    if( selBox.length>0 && selBox[0]>0 ) {
+        let r = selBox[0];
+        let c = selBox[1];
+        if( board[r-1][c]==0) {
+            board[r][c-1] = board[r][c];
+            board[r][c] = 0;             
+
+        }
+    }
     
 }
 function moveDown() {
@@ -64,4 +135,38 @@ function moveRigth() {
     
 }
 
+function moveBox( s, d ){
+    let $S = $(
+        $( '#move-the-box' )[0]
+    .rows[ s[0]]
+        .cells[ s[1] ]
+        );
+        let $D = $(
+            $( '#move-the-box' )[0]
+    .rows[ d[0]]
+        .cells[ d[1] ]
+            );
+            
+        let offS = $S.offset();
+        let offD = $D.offset();
+        let $B = $S[0].className;
+
+        
+
+let offS = $S.offset();
+
+
+let $BOX = $('<div>')
+    .addClass('box')
+   .addClass( b ) 
+    .css( offS )
+    .appendTo('body')
+    $S[0].className="";
+    animationCounter++; 
+$BOX.animate( offD, 3000, function(){
+    $BOX.remove();
+    $D[0].className="$B";
+    animationCounter--;
+});
+};
 drawBoard();
